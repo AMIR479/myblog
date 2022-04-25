118384ccdf4a52674340868ec8ff7d42c467df79
AMIR479 <69789786+AMIR479@users.noreply.github.com>
2022-04-25 07:32:45 +0200

    Added tinymce editor and code review

--- views/layout.php
+++ views/layout.php
@@ -15,12 +15,19 @@
   <link rel="stylesheet" href="<?= SCRIPTS . 'css' . DIRECTORY_SEPARATOR . 'app.css' ?>">
   <link rel="stylesheet" href="<?= SCRIPTS . 'css' . DIRECTORY_SEPARATOR . 'style.css' ?>">
   <link rel="stylesheet" href="<?= SCRIPTS . 'css' . DIRECTORY_SEPARATOR . 'styles.css' ?>">
+  <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+  <script>
+      tinymce.init({
+        selector: '#mytextarea'
+      });
+    </script>
 </head>
 <!-- Barre de navigation -->
 <body>
   <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-relative " id="mainNav" >
     <div class="container mb-3">
-      <a class="navbar-brand" href="/">AMIR BLOG</a>
+      <a class="navbar-brand" href="/">AMIR BLOG </a>
+      <p class="btn btn-success">dev Fullstack</p>
       <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
@@ -47,6 +54,112 @@
   <div>
     <?= $content ?>
   </div>
+
+  <footer class="footer mt-5 pt-0 pb-0">
+    <!-- Footer -->
+<footer class="bg-dark text-center text-white">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!-- Section: Social media -->
+    <section class="mb-4">
+      <!-- Facebook -->
+      <a class="btn btn-outline-light btn-floating m-1" href="https://fr-fr.facebook.com/" role="button"
+        ><i class="fab fa-facebook-f"></i
+      ></a>
+
+      <!-- Twitter -->
+      <a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com/?lang=fr" role="button"
+        ><i class="fab fa-twitter"></i
+      ></a>
+
+        <!-- Linkedin -->
+      <a class="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/uas/login-submit" role="button"
+        ><i class="fab fa-linkedin-in"></i
+      ></a>
+
+      <!-- Github -->
+      <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/AMIR479/myblog" role="button"
+        ><i class="fab fa-github"></i
+      ></a>
+    </section>
+    <!-- Section: Social media -->
+
+    
+    <!-- Section: Form -->
+
+    <!-- Section: Links -->
+    <section class="">
+      <!--Grid row-->
+      <div class="row">
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-1">
+          <h5 class="text-uppercase mb-0">Administation</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+            <li class="nav-item mx-0 mx-lg-1 mt-1"><a class="nav-link py-3 px-0 px-lg-3 rounded " href="/admin/posts">Dashboard</a></li>
+            </li>
+            
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-0 mb-md-0">
+          <h5 class="text-uppercase mb-0">Accueil</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+            <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="/">Accueil</a></li>
+            </li>
+            
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-0 ">
+          <h5 class="text-uppercase mb-0">Tous les posts</h5>
+
+          <ul class="list-unstyled mt-0">
+            <li>
+            <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="/posts">Actualités</a></li>
+
+            </li>
+           
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-0">
+          <h5 class="text-uppercase mb-0">Contact</h5>
+
+          <ul class="list-unstyled mt-0">
+            <li>
+            <li class="nav-item mx-0 mx-lg-1 mt-0"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="/contact">Contact</a></li>
+            </li>
+            
+           
+          </ul>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </section>
+    <!-- Section: Links -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2022 Copyright: Amir
+    
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+  </footer>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </body>
